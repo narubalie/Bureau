f5893b04d76ef1e128691858246f90e212ba7a3d
narubalie <narubalie@gmail.com>
2018-08-23 02:36:52 +0300

    some bugs fixed

--- src/components/organizms/11_SpecialistsCards/index.vue
+++ src/components/organizms/11_SpecialistsCards/index.vue
@@ -1,6 +1,13 @@
 <template lang='pug'>
   el-row
-    el-col(v-for='doctor in doctors' :key='doctor.id' :span='8')
+    el-col(v-for='doctor in doctors'
+    :key='doctor.id'
+    :xs="24"
+    :sm="12"
+    :md="12"
+    :lg="8"
+    :xl="8"
+    )
       router-link(to='/Specialist')
         el-card.doctor(shadow="hover")
           img(:src='doctor.cover')
